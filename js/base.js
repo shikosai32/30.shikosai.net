57ed0309e9d12566360e6ca6e1b136ffe4f448b4
Takeru Osawa <st14d07@gm.ibaraki-ct.ac.jp>
2018-06-20 14:39:28 +0900

    Ver 0.2
    [change]NavBarの位置を上部に固定
    [change]NavBarはWindow Height分スクロールするまでは半透明にした
    [change]ブランドロゴを白抜きのスケーラブルロゴに変更
    [add]NavBarをスクロール時にアニメーション
    [add]トップページの各セクション部
    [change]背景画像をポスターをイメージしたものに変更
    [add]本文フォントにMac用の設定を追加
    [fix]ロゴの位置をWindow Heightに従い中央揃えとなるよう修正

--- js/base.js
+++ js/base.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
   $('.sidenav').sidenav();
+  var flag = false;
+  scrollCond(false);
 });
+
+$(window).scroll(function() {
+  scrollCond(true);
+});
+
+function scrollCond(anim) {
+  nav = $('#navbar');
+  if($(window).scrollTop() <= $(window).height()) {
+    nav.addClass('unscrolled');
+    if(anim) {
+      nav.addClass('anim');
+    } else {
+      nav.removeClass('anim');
+    }
+  } else {
+    nav.removeClass('unscrolled');
+    if(anim) {
+      nav.addClass('anim');
+    } else {
+      nav.removeClass('anim');
+    }
+  }
+}
