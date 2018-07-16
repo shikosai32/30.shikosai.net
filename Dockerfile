397141799d142f83a444378c85c07074f740789d
denebola213 <info@denebola.info>
2018-07-16 09:29:46 +0900

    [add] コンテナ内のリポジトリを更新できるように. ディレクトリ整理

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM nginx
-
-RUN apt-get update
-RUN apt-get install -y git
-RUN git clone -b teaser https://github.com/ibcnit-shikosai/shikosai_30.git /usr/share/teaser
-RUN cp /usr/share/teaser/default.conf /etc/nginx/conf.d/default.conf
---
+++ teaser/update.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ "$#" -ne "1" ]; then
+    echo "Usage: update.sh [DockerID or DockerTAG]"
+    exit
+fi
+
+#docker exec -w /usr/share/nginx/teaser $1 git checkout teaser
+docker exec -w /usr/share/nginx/teaser $1 git pull
+docker exec -w /usr/share/nginx/teaser $1 cp /usr/share/nginx/teaser/teaser.conf /etc/nginx/conf.d/teaser.conf
