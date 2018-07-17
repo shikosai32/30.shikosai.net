4489c9b5b1bee6d71ca3b658909e8b840ba76568
denebola213 <info@denebola.info>
2018-07-17 20:01:58 +0900

    [add] テスト用のデプロイスクリプトを追加

---
+++ deploy_test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#git pull https://github.com/ibcnit-shikosai/shikosai_30.git
+
+str=`docker ps -a | grep shikosai.net`
+if test "${str}" != "" ; then
+    docker stop shikosai.net
+    docker rm shikosai.net
+fi
+
+docker run -d -p 80:80 --name shikosai.net nginx
+docker cp ./ shikosai.net:/usr/share/nginx/30
+docker exec shikosai.net cp /usr/share/nginx/30/teaser.conf /etc/nginx/conf.d/teaser.conf
+docker restart shikosai.net
