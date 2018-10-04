6c59e5058f1c0c832e3ad69a1eb6d7856c831c3c
Takeru Osawa <st14d07@gm.ibaraki-ct.ac.jp>
2018-10-05 02:12:50 +0900

    Ver.1.9 RC

--- teaser/js/base.js
+++ teaser/js/base.js
@@ -1,4 +1,36 @@
 $(document).ready(function(){
   $('.sidenav').sidenav();
+  $('.scrollspy').scrollSpy();
+  $('.collapsible').collapsible();
   $('.materialboxed').materialbox();
+
+  var colors = ['red-border', 'green-border', 'blue-border'];
+  var rand = colors[Math.floor(Math.random() * 3)];
+  $('.table-of-contents').find('a').addClass(rand);
+});
+
+
+
+$('.collapsible-header').click(function() {
+  if($(this).parent().hasClass('active')) {
+    $(this).children('.material-icons').text("keyboard_arrow_down");
+  } else {
+    $(this).children('.material-icons').text("keyboard_arrow_up");
+  }
+});
+$(window).scroll(function() {
+  if($(window).scrollTop() >= 800) {
+    $('#scroll-to-top').fadeIn(100)
+  } else {
+    $('#scroll-to-top').fadeOut(100);
+  }
+});
+
+$('a[href^="#"]').click(function() {
+  var speed = 400;
+  var href = $(this).attr("href");
+  var target = $(href == "#" || href == "" ? 'html' : href);
+  var position = target.offset().top;
+  $('body, html').animate({scrollTop:position-72}, speed, 'swing');
+  return false;
 });
--- teaser/js/load.js
+++ teaser/js/load.js
@@ -1,4 +1,3 @@
 $(function() {
-  // $("header").load("/30/include/header.html");
   $("footer").load("/30/include/footer.html");
 });
